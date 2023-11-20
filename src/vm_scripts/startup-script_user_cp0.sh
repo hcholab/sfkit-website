5776d7e8f0900d2bef83fbf0c49b3f9a7eb1b592
Simon Mendelsohn <simonjmendelsohn@gmail.com>
2023-11-20 11:07:13 -0500

    minor improvement to startup script

--- src/vm_scripts/startup-script_user_cp0.sh
+++ src/vm_scripts/startup-script_user_cp0.sh
@@ -7,7 +7,7 @@ echo $(curl "http://metadata.google.internal/computeMetadata/v1/instance/attribu
 
 # Misc configurations
 sudo sysctl -w net.core.rmem_max=2500000 && sudo sysctl -w net.core.wmem_max=2500000
-sudo ulimit -n 1000000 && sudo ulimit -u 1000000
+sudo bash -c "ulimit -n 1000000 && ulimit -u 1000000"
 export PYTHONUNBUFFERED=TRUE
 
 mkdir -p sfkit && chmod -R 777 sfkit
@@ -21,6 +21,7 @@ do
     sudo docker run --net host --cap-add net_admin \
     -e "SFKIT_API_URL=https://sfkit-website-dev-bhj5a4wkqa-uc.a.run.app/api" \
     -e "SFKIT_PROXY_ON=true" \
+    -e "PYTHONUNBUFFERED=TRUE" \
     -v $PWD/sfkit:/sfkit/.sfkit \
     -v $PWD/auth_key.txt:/sfkit/auth_key.txt:ro \
     us-central1-docker.pkg.dev/dsp-artifact-registry/sfkit/sfkit $cmd
