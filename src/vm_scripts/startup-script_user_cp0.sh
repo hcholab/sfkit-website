9404309d0a3b4653b5a73683794c642d67179ba0
Simon Mendelsohn <simonjmendelsohn@gmail.com>
2023-11-27 09:14:49 -0500

    try sudo to debug startup script

--- src/vm_scripts/startup-script_user_cp0.sh
+++ src/vm_scripts/startup-script_user_cp0.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+sudo -s
+
 if [[ -f startup_was_launched ]]; then exit 0; fi
 touch startup_was_launched
 
 echo $(curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/auth_key" -H "Metadata-Flavor: Google") > auth_key.txt
 
 # Misc configurations
-sudo sysctl -w net.core.rmem_max=2500000 && sudo sysctl -w net.core.wmem_max=2500000
-sudo bash -c "ulimit -n 1000000 && ulimit -u 1000000"
+# sudo sysctl -w net.core.rmem_max=2500000 && sudo sysctl -w net.core.wmem_max=2500000
+# sudo bash -c "ulimit -n 1000000 && ulimit -u 1000000"
+sysctl -w net.core.rmem_max=2500000 && sysctl -w net.core.wmem_max=2500000
+ulimit -n 1000000
+ulimit -u 1000000
 export PYTHONUNBUFFERED=TRUE
 
 mkdir -p sfkit && chmod -R 777 sfkit
 curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh ./get-docker.sh
-sudo docker pull us-central1-docker.pkg.dev/dsp-artifact-registry/sfkit/sfkit # Pull image once
+docker pull us-central1-docker.pkg.dev/dsp-artifact-registry/sfkit/sfkit # Pull image once
 
 cat > run_docker_commands.sh << 'EOF'
 #!/bin/bash
 commands=("auth" "networking --ports 8020,8040" "generate_keys" "run_protocol")
 for cmd in "${commands[@]}"
 do
-    sudo docker run --net host --cap-add net_admin \
+    docker run --net host --cap-add net_admin \
     -e "SFKIT_API_URL=https://sfkit-website-dev-bhj5a4wkqa-uc.a.run.app/api" \
     -e "SFKIT_PROXY_ON=true" \
     -e "PYTHONUNBUFFERED=TRUE" \
