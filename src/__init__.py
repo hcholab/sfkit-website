139a6bf5fd42a5678e7c1ac19e9a499b84f5de6d
Denis Loginov <denis@broadinstitute.org>
2024-01-05 13:07:44 -0500

    Add more logging

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,9 @@ import secrets
 
 import firebase_admin
 import google
+import google.oauth2.id_token
 from google.auth import impersonated_credentials
+from google.auth.transport.requests import Request
 from google.cloud import firestore
 from quart import Quart
 from quart_cors import cors
@@ -59,17 +61,31 @@ def initialize_firebase_app() -> None:
         cred = firebase_admin.credentials.ApplicationDefault()
         if constants.TARGET_SERVICE_ACCOUNT:
             gcred, _ = google.auth.default()
+
+            # for testing
+            aud = "https://iam.googleapis.com"
+            token = google.oauth2.id_token.fetch_id_token(Request(), aud)
+            logger.info(f'Default Google token: {".".join(token.split(".")[:2])}')
+
             gcred = impersonated_credentials.Credentials(
                 source_credentials=gcred,
                 target_principal=constants.TARGET_SERVICE_ACCOUNT,
                 target_scopes=["https://www.googleapis.com/auth/cloud-platform"],
                 lifetime=500)
+
+            # for testing
+            token = google.oauth2.id_token.fetch_id_token(Request(), aud)
+            logger.info(f'Impersonated Google token: {".".join(token.split(".")[:2])}')
+
             # https://github.com/firebase/firebase-admin-python/issues/698
             cred._g_credential = gcred
             cred._project_id = constants.FIREBASE_PROJECT_ID
-            token = cred.get_access_token().access_token
-            logger.info(f'Impersonated token: {".".join(token.split(".")[:2])}')
             options['serviceAccountId'] = constants.TARGET_SERVICE_ACCOUNT
+
+            # for testing
+            token = cred.get_access_token().access_token
+            logger.info(f'Impersonated Firebase token: {".".join(token.split(".")[:2])}')
+
         firebase_admin.initialize_app(credential=cred, options=options)
 
     # test firestore connection
