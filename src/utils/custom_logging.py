4943e19eb1a3f23d96e46c1b842a0849e33a6ab9
Denis Loginov <denis@broadinstitute.org>
2024-01-09 16:56:12 -0500

    Fix log format and level for Terra/Kubernetes

--- src/utils/custom_logging.py
+++ src/utils/custom_logging.py
@@ -7,18 +7,19 @@ from src.utils import constants
 
 
 def setup_logging(name: Optional[str] = None) -> logging.Logger:
-    if constants.TERRA or constants.CLOUD_RUN.lower() == "true":
+    level = logging.getLevelName(constants.LOG_LEVEL)
+
+    # If the environment variable is set to "True", we are running on Cloud Run
+    if constants.CLOUD_RUN.lower() == "true":
         # Instantiate the Google Cloud Logging client
         client = gcp_logging.Client()
 
         # Attach the Cloud Logging handler to the root logger
         client.get_default_handler()
-
-        log_level = logging.getLevelName(constants.LOG_LEVEL)
-        client.setup_logging(log_level=log_level)
-        logging.log(logging.INFO, "Logging initialized, LOG_LEVEL=%s(%d)", constants.LOG_LEVEL, log_level)
+        client.setup_logging(log_level=level)
     else:
-        # For local development, log to stdout with a simple format
-        logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        # For Kubernetes or local development, log to stdout with a simple format
+        logging.basicConfig(level=level, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
+    logging.log(logging.INFO, "Logging initialized, LOG_LEVEL=%s(%d)", constants.LOG_LEVEL, level)
     return logging.getLogger(name)
