72a632d03982e2037fcfb5cb2524208d2ca2cb71
Denis Loginov <137337+dinvlad@users.noreply.github.com>
2024-01-09 17:53:24 -0500

    Step up DEBUG logging level to avoid too much verbosity (#54)
    * Step up DEBUG logging level to avoid too much verbosity
    
    * Return an instance of Logger subclass

--- src/utils/custom_logging.py
+++ src/utils/custom_logging.py
@@ -6,8 +6,29 @@ from google.cloud import logging as gcp_logging
 from src.utils import constants
 
 
-def setup_logging(name: Optional[str] = None) -> logging.Logger:
+class Logger(logging.Logger):
+    # used to avoid too much verbosity from third-party libraries
+    DEBUG = logging.DEBUG + 1
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    @classmethod
+    def from_super(cls, super_instance: logging.Logger):
+        # Create a new instance of Logger
+        instance = cls(super_instance.name)
+        # Copy the state from the superclass instance
+        instance.__dict__.update(super_instance.__dict__)
+        return instance
+
+    def debug(self, msg: str, *args, **kwargs) -> None:
+        super().log(Logger.DEBUG, msg, *args, **kwargs)
+
+
+def setup_logging(name: Optional[str] = None) -> Logger:
     level = logging.getLevelName(constants.LOG_LEVEL)
+    if level == logging.DEBUG:
+        level = Logger.DEBUG
 
     # If the environment variable is set to "True", we are running on Cloud Run
     if constants.CLOUD_RUN.lower() == "true":
@@ -22,4 +43,6 @@ def setup_logging(name: Optional[str] = None) -> logging.Logger:
         logging.basicConfig(level=level, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
     logging.log(logging.INFO, "Logging initialized, LOG_LEVEL=%s(%d)", constants.LOG_LEVEL, level)
-    return logging.getLogger(name)
+
+    logger = logging.getLogger(name)
+    return Logger.from_super(logger)
