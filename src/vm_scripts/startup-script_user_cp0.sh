d8e64bfccf72321d7d255b394214b7559e53ac93
Denis Loginov <137337+dinvlad@users.noreply.github.com>
2025-12-01 15:32:43 -0500

    Fix Docker command in cp0 startup script

--- src/vm_scripts/startup-script_user_cp0.sh
+++ src/vm_scripts/startup-script_user_cp0.sh
@@ -34,21 +34,13 @@ if [ $attempt -eq $max_attempts ]; then
   exit 1
 fi
 
-docker pull us-central1-docker.pkg.dev/dsp-artifact-registry/sfkit/sfkit # Pull image once
-
 cat > run_docker_commands.sh << 'EOF'
 #!/bin/bash
-commands=("auth" "networking --ports 8020,8040" "generate_keys" "run_protocol")
-for cmd in "${commands[@]}"
-do
-    docker run --net host --cap-add net_admin \
-    -e "SFKIT_API_URL=$SFKIT_API_URL" \
-    -e "SFKIT_PROXY_ON=true" \
-    -e "PYTHONUNBUFFERED=TRUE" \
+docker run --pull always \
+    -e SFKIT_API_URL \
     -v $PWD/sfkit:/sfkit/.sfkit \
     -v $PWD/auth_key.txt:/sfkit/auth_key.txt:ro \
-    us-central1-docker.pkg.dev/dsp-artifact-registry/sfkit/sfkit $cmd
-done
+    ghcr.io/hcholab/sfkit run
 EOF
 
 chmod +x run_docker_commands.sh
