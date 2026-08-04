8ab27d3f5a91904290ec6ce4d3eb823abc488f48
Denis Loginov <denis@stratologics.com>
2026-08-03 21:13:47 -0400

    Output backend URLs from Terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "frontend_service_account" {
   description = "Service account email for the frontend Cloud Run service"
   value       = google_service_account.frontend.email
 }
+
+output "urls" {
+  description = "Backend service URLs"
+  value       = google_cloud_run_v2_service.website.urls
+}
